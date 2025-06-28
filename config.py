7c25b1858a77af47a443a9afa6c1d52b232bae51
Emanoel Lopes <emanoelopes@gmail.com>
2025-06-28 16:13:29 -0300

    boostrap conf

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    UPLOAD_FOLDER = 'uploads'
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_FOLDER = os.path.join(BASEDIR, 'uploads')
