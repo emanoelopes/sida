5b4cf98a158b7275dc9abb080833c557eebf728a
Emanoel Lopes <emanoelopes@gmail.com>
2025-09-19 22:19:22 -0300

    flask

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     UPLOAD_FOLDER = os.path.join(BASEDIR, 'uploads')
