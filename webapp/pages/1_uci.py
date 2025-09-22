0cb7b45b209a817ee65319e35fd7edad94631ab1
Emanoel Lopes <emanoelopes@gmail.com>
2025-09-22 20:10:27 -0300

    streamlit pages

--- webapp/pages/1_uci.py
+++ webapp/pages/1_uci.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 import os
 
-from utilidades import leitura_de_dados
+from utilidades import leitura_uci_data
 
-df_stinfo = st.session_state['dataframes']['studentInfo']
+df_stinfo = st.session_state['dataframes_uci']['portuguese']
 
 st.dataframe(df_stinfo)
--- webapp/utilidades.py
+++ webapp/utilidades.py
@@ -3,41 +3,6 @@ import streamlit as st
 import pandas as pd
 import os
 
-def leitura_de_dados():
-    """
-    Carrega todos os arquivos CSV do diretório datasets/oulad_data em dataframes.
-    
-    Returns:
-        dict: Dicionário com os dataframes carregados, onde a chave é o nome do arquivo sem extensão
-    """
-    if not 'dataframes' in st.session_state:
-        # Definir o caminho para o diretório dos datasets
-        dataset_path = Path(__file__).parents[1] / 'datasets' / 'oulad_data'
-        
-        # Verificar se o diretório existe
-        if not dataset_path.exists():
-            st.error(f"Diretório não encontrado: {dataset_path}")
-            return {}
-            
-        st.info(f"Carregando dados de: {dataset_path}")
-
-        # Dicionário para armazenar os dataframes
-        dataframes = {}
-
-        # Percorrer os arquivos no diretório
-        for filename in os.listdir(dataset_path):
-            if filename.endswith('.csv'):
-                file_path = os.path.join(dataset_path, filename)
-                df_name = os.path.splitext(filename)[0]
-                try:
-                    df = pd.read_csv(file_path, sep=',', encoding='ISO-8859-1')
-                    if df.empty:
-                        st.warning(f"Arquivo '{filename}' está vazio.")
-                        continue
-                    dataframes[df_name] = df
-                    st.success(f"Arquivo '{filename}' carregado com sucesso.")
-                except Exception as e:
-                    st.error(f"Erro ao carregar '{filename}': {str(e)}")
-
-        st.session_state['dataframes'] = dataframes
-    return st.session_state['dataframes']
+def leitura_oulad_data():
+    datasets_path = Path(__file__).parent.parents / 'datasets' / 'oulad_data'
+    st.write(f"Path dos datasets: {datasets_path}")
